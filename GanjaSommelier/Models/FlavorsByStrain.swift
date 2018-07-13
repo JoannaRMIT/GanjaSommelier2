9c4fe5620105338b216e32447fc205d346bb90b5
joshimmaraj <joshimmaraj@gmail.com>
2018-07-13 18:11:37 +1000

    starting flavour function

---
+++ GanjaSommelier/Models/FlavorsByStrain.swift
@@ -0,0 +1,14 @@
+//
+//  FlavorsByStrain.swift
+//  GanjaSommelier
+//
+//  Created by Default on 13/7/18.
+//  Copyright © 2018 Joanna Ingram. All rights reserved.
+//
+
+import Foundation
+// this API request returns a simple array from the strain id, so no JSON deserializing for the full version
+func searchFlavorsByStrain(id: Int)
+{
+    
+}

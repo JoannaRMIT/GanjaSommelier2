f4963a595d3f96cad044e9aca3e52e9ec4188224
joshimmaraj <joshimmaraj@gmail.com>
2018-07-14 00:41:15 +1000

    effects model

--- GanjaSommelier/Models/EffectsByStrain.swift
+++ GanjaSommelier/Models/EffectsByStrain.swift
@@ -19,7 +19,7 @@ struct Effect: Decodable
     var type: String
 }
 
-func searchEffectsByStrain() -> [Effect]?
+func decodeEffectsJSON() -> [Effect]?
 {
     if let url = Bundle.main.url(forResource: "effects", withExtension: "json")
     {
@@ -28,11 +28,7 @@ func searchEffectsByStrain() -> [Effect]?
             let data = try Data(contentsOf: url)
             let decoder = JSONDecoder()
             let effectsData = try decoder.decode(ResponseData.self, from: data)
-            let decodedEffects = effectsData.effects
-            
-            
-            
-            
+            return effectsData.effects
         }
         catch
         {
@@ -40,4 +36,45 @@ func searchEffectsByStrain() -> [Effect]?
         }
     }
     return nil
+}
+
+
+
+func sortEffects(effectType: String) -> [String]
+{
+    var allEffects = decodeEffectsJSON()
+    var positiveEffectsArray = [String]()
+    var negativeEffectsArray = [String]()
+    var medicalArray = [String]()
+    
+    for i in 0...allEffects!.count
+    {
+        if allEffects![i].type == "positive"
+        {
+            positiveEffectsArray.append(allEffects![i].effect)
+        }
+        if allEffects![i].type == "negative"
+        {
+            negativeEffectsArray.append(allEffects![i].effect)
+        }
+        if allEffects![i].type == "medical"
+        {
+            medicalArray.append(allEffects![i].effect)
+        }
+    }
+    var output = [String]()
+    let switchVal = effectType
+    switch switchVal
+    {
+    case "positive":
+        output = positiveEffectsArray
+    case "negative":
+        output = negativeEffectsArray
+    case "medicinal":
+        output = medicalArray
+    default:
+        print("error: input can only be 'positive', 'negative' or 'medicinal'")
+        break
+    }
+    return output
 }

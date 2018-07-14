c70ee349727cdce0e9637e9a72bc1d692aa2fbf4
joshimmaraj <joshimmaraj@gmail.com>
2018-07-14 13:01:54 +1000

    fixed up the flavour and effects models

--- GanjaSommelier/Models/FlavorsByStrain.swift
+++ GanjaSommelier/Models/FlavorsByStrain.swift
@@ -7,31 +7,33 @@
 //
 
 import Foundation
-// this API request returns a simple array from the strain id, so no JSON deserializing for the mock up version
+// hardcoded array for the mockup and reused the effects function
 
 var flavors = ["Earthy","Chemical","Pine","Spicy/Herbal","Pungent","Pepper","Flowery","Citrus","Orange","Sweet","Skunk","Grape","Minty","Woody","Cheese","Diesel","Tropical","Grapefruit","Nutty","Lemon","Berry","Blueberry","Ammonia","Apple","Rose","Butter","Honey","Tea","Lime","Lavender","Strawberry","Mint","Chestnut","Tree Fruit","Pear","Apricot","Peach","Blue Cheese","Menthol","Coffee","Tar","Mango","Pineapple","Sage","Vanilla","Plum","Tobacco","Violet"]
 
 
 func pickStrainFlavors() -> [String]
 {
-    var pickedFlavors = [String]()
-    var rand1: Int
-    var rand2: Int
-    var rand3: Int
-    rand1 = Int(arc4random_uniform(UInt32(flavors.count)))
-    repeat
-    {
-        rand2 = Int(arc4random_uniform(UInt32(flavors.count)))
-    }
-        while rand2 == rand1
+    return getEffects(flavors, 3)
     
-    repeat
-    {
-        rand3 = Int(arc4random_uniform(UInt32(flavors.count)))
-    }
-        while (rand3 == rand1) || (rand3 == rand2)
-    
-    pickedFlavors = [flavors[rand1], flavors[rand2], flavors[rand3]]
-    return pickedFlavors
+//    var pickedFlavors = [String]()
+//    var rand1: Int
+//    var rand2: Int
+//    var rand3: Int
+//    rand1 = Int(arc4random_uniform(UInt32(flavors.count)))
+//    repeat
+//    {
+//        rand2 = Int(arc4random_uniform(UInt32(flavors.count)))
+//    }
+//        while rand2 == rand1
+//
+//    repeat
+//    {
+//        rand3 = Int(arc4random_uniform(UInt32(flavors.count)))
+//    }
+//        while (rand3 == rand1) || (rand3 == rand2)
+//
+//    pickedFlavors = [flavors[rand1], flavors[rand2], flavors[rand3]]
+//    return pickedFlavors
     
 }

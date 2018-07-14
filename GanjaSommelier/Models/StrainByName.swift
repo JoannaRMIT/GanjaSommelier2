242ebabd0a91eb7685607ca3221855bca35edff8
joshimmaraj <joshimmaraj@gmail.com>
2018-07-15 00:22:34 +1000

    couple new strain structs

---
+++ GanjaSommelier/Models/StrainByName.swift
@@ -0,0 +1,9 @@
+//
+//  StrainByName.swift
+//  GanjaSommelier
+//
+//  Created by Default on 14/7/18.
+//  Copyright © 2018 Joanna Ingram. All rights reserved.
+//
+
+import Foundation
--- GanjaSommelier/Models/StrainStructs.swift
+++ GanjaSommelier/Models/StrainStructs.swift
@@ -8,40 +8,77 @@
 
 import Foundation
 
-struct StrainDetail
+struct StrainInformation
 {
     var name: String
+    var flavours: [String]
     var race: String
     var matchWith: String
     var medicinalUse: [String]
     var positiveEffects: [String]
-    var negativeEffects: [String]
+    var negativeEffects: String
 }
 
-struct StrainDetailsList
+//after all that thinking of structs and ids i just went
+//simple with a dictionary for searched strains, and array
+//for favourited strains. If it's in the favourites array,
+//it'll be in the stored strains too with it's information
+//and can be accessed with just it's name
+
+var storedStrains = [String: StrainInformation]()
+var favouriteStrains = [String]()
+
+//building the StrainInformation
+func buildStrainDetails(_ strainName: String) -> StrainInformation
 {
-    var searchedStrainsList:[StrainDetail]
-    init()
-    {
-        searchedStrainsList = [StrainDetail]()
-    }
+    let flavoursBuild = getDetails(flavors, 3)
+    let raceBuild = getDetails(race, 1)
+    let matchWithBuild = getDetails(matchWith, 1)
+    let medicinalUseBuild = getDetails(medicinal, 3)
+    let positiveEffectsBuild = getDetails(positive, 4)
+    let negativeEffectsBuild = getDetails(negative, 1)
     
-    mutating func addStrainToList(_ strainName: StrainDetail)
+    let builtStrainInformation = StrainInformation(name: strainName, flavours: flavoursBuild,race: raceBuild[0], matchWith: matchWithBuild[0], medicinalUse: medicinalUseBuild, positiveEffects: positiveEffectsBuild, negativeEffects: negativeEffectsBuild[0])
+    
+    return builtStrainInformation
+}
+
+//storing Strain Information, checking beforehand if it's already been stored
+
+func storeNewStrain(_ strainToStore: StrainInformation)
+{
+    if let _ = storedStrains[strainToStore.name]
     {
-        searchedStrainsList.append(strainName)
+        print("strains information is already stored")
+    } else
+    {
+        storedStrains[strainToStore.name] = strainToStore
+    }
+}
+//adding to favourites
+
+func addToFavs(_ newFavouriteStrain: StrainInformation)
+{
+    if favouriteStrains.contains(newFavouriteStrain.name)
+    {
+        print("strain already favourited")
+    } else
+    {
+        favouriteStrains.append(newFavouriteStrain.name)
     }
 }
 
-struct FavouriteStrainsList
+
+//fetching Strain Information, unless strain not stored yet
+
+func getStoredStrain(_ strainToFetch: String) -> StrainInformation?
 {
-    var favouriteStrains: [StrainDetail]
-    init()
+    if let getStrain = storedStrains[strainToFetch]
     {
-        favouriteStrains = [StrainDetail]()
-    }
-    
-    mutating func addStrainToFavourites(_ strainName: StrainDetail)
+        return getStrain
+    } else
     {
-        favouriteStrains.append(strainName)
+        print("strain not found")
+        return nil
     }
 }

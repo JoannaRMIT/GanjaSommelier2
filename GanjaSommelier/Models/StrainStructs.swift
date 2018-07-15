4a829a1c13059498329e8dba6ccccc63f375a490
joshimmaraj <joshimmaraj@gmail.com>
2018-07-15 16:50:11 +1000

    pulled together the dishes and strains structs

--- GanjaSommelier/Models/StrainStructs.swift
+++ GanjaSommelier/Models/StrainStructs.swift
@@ -19,64 +19,83 @@ struct StrainInformation
     var negativeEffects: String
 }
 
-//dictionary for searched strains with all their details, array for favourites names
-//easy lookup for details using just names
+//use searchedStrains for our collection
+var searchedStrains = ListOfStrains()
 
-var storedStrains = [String: StrainInformation]()
-var favouriteStrains = [String]()
+//.storedStrains["strainName"] = info (dictionary collection, with names as keys)
+//.favouriteStrains = array of favourites
+//.showInfo("strainName") = shows stored info, or creates new info and stores it. use for info from search results
+//.addToFavs("strainName") -> adds to favouriteStrains
+//.showStoredInfo("strainName") = only shows stored info. use for fetching favourite strains info
 
-//building the StrainInformation
-func buildStrainDetails(_ strainName: String) -> StrainInformation
+struct ListOfStrains
 {
-    let flavoursBuild = getFlavors(strainName)! //fixed to get the flavours matching the searches
-    let raceBuild = getDetails(race, 1)
-    let matchWithBuild = getDetails(matchWith, 1)
-    let medicinalUseBuild = getDetails(medicinal, 3)
-    let positiveEffectsBuild = getDetails(positive, 4)
-    let negativeEffectsBuild = getDetails(negative, 1)
+    //dictionary for searched strains with all their details (key: name, value: Info), array for favourites names
+    
+    var storedStrains = [String: StrainInformation]()
+    var favouriteStrains = [String]()
+    
+    //shows up stored strain info, or creates a new profile if it's new
+
+    mutating func showInfo(_ strainName: String) -> StrainInformation
+    {
+        if let alreadyStoredStrain = storedStrains[strainName]
+        {
+            return alreadyStoredStrain
+            
+        } else
+        {
+            return buildStrainDetails(strainName)
+        }
+    }
+    
+    //builds the new profile, returns info and stores to the list
+    mutating func buildStrainDetails(_ strainName: String) -> StrainInformation
+    {
+        let flavoursBuild = getFlavors(strainName)! //fixed to get the flavours matching the searches
+        let raceBuild = getDetails(race, 1)
+        let matchWithBuild = getDetails(matchWith, 1)
+        let medicinalUseBuild = getDetails(medicinal, 3)
+        let positiveEffectsBuild = getDetails(positive, 4)
+        let negativeEffectsBuild = getDetails(negative, 1)
+        
+        
+        let builtStrainInformation = StrainInformation(name: strainName, flavours: flavoursBuild,race: raceBuild[0], matchWith: matchWithBuild[0], medicinalUse: medicinalUseBuild, positiveEffects: positiveEffectsBuild, negativeEffects: negativeEffectsBuild[0])
+        
+        storedStrains[builtStrainInformation.name] = builtStrainInformation
+        
+        return builtStrainInformation
+    }
     
     
-    let builtStrainInformation = StrainInformation(name: strainName, flavours: flavoursBuild,race: raceBuild[0], matchWith: matchWithBuild[0], medicinalUse: medicinalUseBuild, positiveEffects: positiveEffectsBuild, negativeEffects: negativeEffectsBuild[0])
+    //adding to favourites
     
-    return builtStrainInformation
-}
-
-//storing Strain Information, checking beforehand if it's already been stored
-
-func storeNewStrain(_ strainToStore: StrainInformation)
-{
-    if let _ = storedStrains[strainToStore.name]
+    mutating func addToFavs(_ newFavouriteStrain: StrainInformation)
     {
-        print("strains information is already stored")
-    } else
+        if favouriteStrains.contains(newFavouriteStrain.name)
+        {
+            print("strain already favourited")
+            
+        } else
+        {
+            favouriteStrains.append(newFavouriteStrain.name)
+        }
+    }
+    
+    
+    //fetching favourites strain profile from storedStrains
+    
+    func showStoredInfo(_ strainToFetch: String) -> StrainInformation?
     {
-        storedStrains[strainToStore.name] = strainToStore
+        if let getStrain = storedStrains[strainToFetch]
+        {
+            return getStrain
+            
+        } else
+        {
+            print("strain not found")
+            return nil
+        }
     }
 }
-//adding to favourites
 
-func addToFavs(_ newFavouriteStrain: StrainInformation)
-{
-    if favouriteStrains.contains(newFavouriteStrain.name)
-    {
-        print("strain already favourited")
-    } else
-    {
-        favouriteStrains.append(newFavouriteStrain.name)
-    }
-}
-
-
-//fetching Strain Information, unless strain not stored yet
-
-func getStoredStrain(_ strainToFetch: String) -> StrainInformation?
-{
-    if let getStrain = storedStrains[strainToFetch]
-    {
-        return getStrain
-    } else
-    {
-        print("strain not found")
-        return nil
-    }
-}
--- GanjaSommelier/Models/dishStructs.swift
+++ GanjaSommelier/Models/dishStructs.swift
@@ -18,21 +18,61 @@ struct DishInformation
     var rating: Int
 }
 
-var dishList = [String: DishInformation]()
+//use storedDishes for our collection
+var storedDishes = ListOfDishes()
 
-//storing/adding
+//.list["dishName"] returns DishInformation in list dictionary
+//.getDish("dishName") same but checks first i.e. optional
+//.createDish(add all the info) to store to list
+//.editDish(oldDishName, all the new info) delete oldDish, store the new one
 
-func storeNewDish(_ newDishName: DishInformation)
+struct ListOfDishes
 {
-    if let _ = dishList[newDishName.name]
+    //dictionary for the list
+    var list = [String: DishInformation]()
+    
+    //create
+    mutating func createDish(name: String, strain: String, notes: String, img: UIImage, rating: Int)
     {
-        print("dish has already been added, or it's name has already been used")
-    } else {
-        dishList[newDishName.name] = newDishName
+        let newDish: DishInformation = DishInformation(name: name, strain: strain, notes: notes, img: img, rating: rating)
+        if let _ = list[newDish.name]
+        {
+            print("dish already stored")
+            
+        } else {
+            
+            list[newDish.name] = newDish
+            
+        }
     }
+    
+    //fetch
+    func getDish(_ dishName: String) -> DishInformation?
+    {
+        if let fetchDish = list[dishName]
+        {
+            return fetchDish
+            
+        } else {
+            
+            print("dish not found")
+            return nil
+            
+        }
+    }
+    
+    //edit
+    mutating func editDish(_ dishName: String, newName: String, newStrain: String, newNotes: String, newImg: UIImage, newRating: Int)
+    {
+        if let _ = list[dishName]
+        {
+            let editedDish = DishInformation(name: newName, strain: newStrain, notes: newNotes, img: newImg, rating: newRating)
+            list[dishName] = nil //delete the old dish before storing the new version
+            list[editedDish.name] = editedDish
+        } else {
+            print("dish not found")
+        }
+        
+    }
+
 }
-//do I need a function to create a dish object?
-
-//editing a dish
-
-
